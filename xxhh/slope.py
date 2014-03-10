798c1daf4f99e35f6a0320286559ecf9d2f2cae1
janson <gandancing@gmail.com>
2014-03-10 08:34:30 -0400

    results

--- xxhh/slope.py
+++ xxhh/slope.py
@@ -1,4 +1,4 @@
-import sys, os, os.path
+import sys, os, os.path, re
 import codecs
 from scipy.sparse import *
 from scipy import *
@@ -10,8 +10,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'xxhh.settings'
 
 from django.db.models import Count
 from django.db.models import Q
-from xxhh.models import TestLog as XhLogUd
-#from xxhh.models import XhLogUd
+#from xxhh.models import TestLog as XhLogUd
+from xxhh.models import XhLogUd
 
 diff = lil_matrix( (100000,100000), dtype=float)
 diff[1,2] = 8.0
@@ -47,19 +47,19 @@ def input_test():
 #for obj in XhLogUd.objects.filter(post_id=67400):
 #    print obj.post_id, obj.guid, obj.uaction
 
-#for obj in XhLogUd.objects.filter(guid='11a2458d-6a38-ccde-e176-af0971b26daf'):
+#print len(XhLogUd.objects.filter(guid=''))
 #    print obj.post_id, obj.guid, obj.uaction
 
 #print len(XhLogUd.objects.values("guid").distinct())
 #print len(XhLogUd.objects.values("post_id").distinct())
 
 RATE = {}
-RATE['u'] = 3.0
-RATE['d'] = 1.0
+RATE['u'] = 3
+RATE['d'] = 1
 
 class Ratings(object):
     def __init__(self):
-        all_objs = list(XhLogUd.objects.all())
+        all_objs = list(XhLogUd.objects.exclude(guid=''))
         guid2id = {}
         id2guid = []
         i = 0
@@ -92,8 +92,27 @@ class Ratings(object):
             guids[guid2id[obj.guid]].append((RATE[obj.uaction.strip()], self.guid2id[obj.guid], self.post2id[obj.post_id]))
         self.guids = guids
 
+        print "all users", len(self.id2guid)
+        print "all items", len(self.id2post)
+        
+        #sorting users
+        sorted_guids = [(g,len(self.guids[self.guid2id[g]]) ) for i,g in enumerate(self.id2guid)]
+        self.sorted_guids = [g for g,c in sorted(sorted_guids, key = lambda item: -item[1]) if c >= 3]
+        print self.sorted_guids[0:10]
+        print 'len and max rated guid', len(self.sorted_guids), self.sorted_guids[0], len(self.guids[self.guid2id[self.sorted_guids[0]]])
+
     def by_user(self, id):
         return self.guids[id]
+
+    def all_users(self):
+        for guid in self.sorted_guids:
+            yield self.guid2id[guid]
+
+    def for_mrjob(self, filename):
+        with open(filename,'w') as f:
+            for user_id in self.all_users():
+                for rate in self.by_user(user_id):
+                    f.write('%s %d %d\n' % (rate[1], rate[2], rate[0]))
 
 ratings = Ratings()
 print 'Ratings created'
@@ -197,7 +216,8 @@ class WeightSlopeOne(object):
     def train(self):
         self.init_model()
 
-        for user_id in xrange(len(self.ratings.id2guid)):
+        #for user_id in xrange(len(self.ratings.id2guid)):
+        for user_id in self.ratings.all_users():
             #print 'Begin train for user %s len=%d' % (user_id, len(self.ratings.by_user(user_id)))
             for rate1 in self.ratings.by_user(user_id):
                 for rate2 in self.ratings.by_user(user_id):
@@ -232,15 +252,74 @@ class WeightSlopeOne(object):
             return 0
         return prediction / freqs
 
+    def perdict_user(self, user_id):
+        rlts = []
+        guid_set = set(self.ratings.guids[user_id])
+        for item in self.ratings.id2post:
+            if item not in guid_set:
+                item_id = self.ratings.post2id[item]
+                if self.can_predict(user_id, item_id):
+                    rlts.append((user_id, item_id, self.perdict(user_id, item_id)))
+        return sorted(rlts, key=lambda item: -item[2])
+
     def save(self, filename):
         _ = joblib.dump(self.diff_like, filename+'.diff', compress=9)
         _ = joblib.dump(self.freq_like, filename+'.freq', compress=9)
 
+    def load(self, filename):
+        self.diff_like = joblib.load(filename+'.diff')
+        self.freq_like = joblib.load(filename+'.freq')
+
 #slope = BiPolarSlopeOne(ratings)
 slope = WeightSlopeOne(ratings)
-slope.train()
-slope.save('out')
+#slope.train()
 print 'train complete'
+#slope.save('out3')
+#print 'saved'
+
+#slope.load('out')
+
+'''
+rlts = slope.perdict_user(ratings.guid2id[ratings.sorted_guids[0]])
+with open('guid1.out', 'w') as f:
+    for r in rlts:
+        f.write('%s %d %f\n' % ( ratings.id2guid[r[0]], ratings.id2post[r[1]], r[2]) )
+'''
+
+'''
+with open('guid2.out', 'w') as f:
+    for rate in ratings.by_user(ratings.guid2id[ratings.sorted_guids[0]]):
+        f.write('%s %d %f\n' % (ratings.id2guid[rate[1]], ratings.id2post[rate[2]], rate[0]))
+'''
+
+#ratings.for_mrjob('mrjob.out')
+
+def convert2mrjob():
+    f1 = open('guid0.out', 'r')
+    f2 = open('mrjob_perdict.out', 'w')
+    for line in f1:
+        its = line.split()
+        f2.write("%s %s\n" % (ratings.guid2id[its[0]], ratings.post2id[int(its[1])]) )
+    f1.close()
+    f2.close()
+
+def from_mrjob():
+    f1 = open('mrjob_rlt.out', 'r')
+    f2 = open('mrjob_rlt2.out', 'w')
+    rlts = []
+    for l in f1:
+        line = re.sub(r'[\[\],"]', '', l)
+        items = line.split()
+        assert(len(items) == 3)
+        rlts.append((ratings.id2guid[int(items[0])], ratings.id2post[int(items[1])], float(items[2])) )
+    rlts = sorted(rlts, key=lambda item: -item[2])
+    for r in rlts:
+        f2.write("%s %d %f\n" % r)
+
+    f2.close()
+    f1.close()
+
+from_mrjob()
 
 #print ratings.guid2id[u'C'], ratings.post2id[2]
 #print slope.perdict(ratings.guid2id[u'C'], ratings.post2id[2])
